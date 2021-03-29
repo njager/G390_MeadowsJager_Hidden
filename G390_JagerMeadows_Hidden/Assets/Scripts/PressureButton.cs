3aabbe92efc323757f03f7742c2507b3c13e9227
njager <nicholasejager@gmail.com>
2021-03-29 19:03:23 -0400

    coded the frick out of buttons

---
+++ G390_JagerMeadows_Hidden/Assets/Scripts/PressureButton.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressureButton : MonoBehaviour
+{
+    //public variables
+    public Material defaultMaterial;
+    public Material secondMaterial;
+    public MeshRenderer subject;
+
+    //private variables
+    bool isDefaultMat;
+    bool isSecondMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set initial material
+        subject.material = defaultMaterial;
+        isDefaultMat = true;
+    }
+
+    //when something enters the trgger
+    private void OnTriggerEnter(Collider other)
+    {
+        //if it's the player
+        if (other.CompareTag("Player"))
+        {
+            //and if the current mat is default, switch to the second mat
+            if (isDefaultMat == true)
+            {
+                subject.material = secondMaterial;
+                isDefaultMat = false;
+            }
+            //but if the current mat is the second mat, switch to default
+            else if (isDefaultMat == false)
+            {
+                subject.material = defaultMaterial;
+                isDefaultMat = true;
+            }
+        }
+    }
+    //same as above but when the player exits
+    private void OnTriggerExit(Collider other)
+    {
+        //if it's the player
+        if (other.CompareTag("Player"))
+        {
+            //and if the current mat is default, switch to the second mat
+            if (isDefaultMat == true)
+            {
+                subject.material = secondMaterial;
+                isDefaultMat = false;
+            }
+            //but if the current mat is the second mat, switch to default
+            else if (isDefaultMat == false)
+            {
+                subject.material = defaultMaterial;
+                isDefaultMat = true;
+            }
+        }
+    }
+}
