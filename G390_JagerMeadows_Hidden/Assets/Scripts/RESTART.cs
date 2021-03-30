5c268b89a848b0914b49b5b98c74d200577d3e87
aemeadow <aemeadow@iu.edu>
2021-03-30 03:59:11 -0400

    checkpoints
    checkpoints r dead oops

---
+++ G390_JagerMeadows_Hidden/Assets/Scripts/RESTART.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RESTART : MonoBehaviour
+{
+    public GameObject Player;
+    
+
+    public void OnTriggerEnter(Collider col)
+    {
+        Player.transform.position = CheckPoint.GetActiveCheckPointPosition();
+    }
+}
