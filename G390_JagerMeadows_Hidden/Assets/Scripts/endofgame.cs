39647b5f609b7207dfce90630c742035c9077280
aemeadow <aemeadow@iu.edu>
2021-03-30 07:25:13 -0400

    done with end script
    scale got out of hand,, edit as you feel is necessary

---
+++ G390_JagerMeadows_Hidden/Assets/Scripts/endofgame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endofgame : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Application.Quit();
+        Debug.Log("Game has ended");
+    }
+}
+
